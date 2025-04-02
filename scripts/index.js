986117973bd73a5c080e9a69be7920466acb4fe2
BRITGAINES <GAINES_BRITNEY@YAHOO.COM>
2025-04-02 17:09:43 -0500

    Updated Stage 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const editProfileBtn = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+
+const newPostBtn = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
+
 const initialCards = [
   {
     name: "Val Thorens",
